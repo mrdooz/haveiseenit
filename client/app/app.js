502403e9f676a53edccf632dcb325e01bad1fa0b
Magnus Österlind <magnus.osterlind@gmail.com>
2015-02-27 13:00:27 -0800

    Initial client commit

---
+++ client/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.series',
+  'ui.bootstrap'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/series'});
+}]);
---
+++ client/app/series/series.js
@@ -0,0 +1,82 @@
+'use strict';
+
+angular.module('myApp.series', ['ngRoute'])
+
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/series', {
+        templateUrl: 'series/series.html',
+        controller: 'SeriesCtrl'
+      });
+    }])
+
+    .controller('SeriesCtrl', ['$scope', '$http', function($scope, $http) {
+
+      $scope.episodes = {}
+      $scope.episodesById = {}
+
+      $http.get('http://localhost:8080/series').success(function(data) {
+        $scope.series = data.series;
+        _.each($scope.series, function(elem) {
+          elem.seasons = _.range(1, elem.num_seasons + 1);
+        });
+      });
+
+      $scope.onTitleClick = function(seriesId) {
+      };
+
+      $scope.onSeasonClick = function(seriesId, seasonNum) {
+        // When a season is clicked, download the episodes, if they aren't already downloaded
+        if (!$scope.episodes[seriesId] || !$scope.episodes[seriesId][seasonNum] || $scope.episodes[seriesId][seasonNum].length === 0) {
+          $http.get('http://localhost:8080/series/' + seriesId + '/' + seasonNum + '/episodes').success(function(data) {
+
+            var currentTime = new Date();
+
+            if (!$scope.episodes[seriesId]) {
+              $scope.episodes[seriesId] = {};
+              $scope.episodesById[seriesId] = {};
+            }
+
+            // store the episodes in a sorted array, and set the seen/available flags
+            var episodes = [];
+            var episodeMap = {};
+            _.each(data.episodes, function(val) {
+              val.seen = _.contains(data.seen, val.episode);
+              val.available = currentTime > new Date(val.airdate);
+              episodes.push(val);
+              episodeMap[val.episode] = val;
+            });
+
+            $scope.episodes[seriesId][seasonNum] = _.sortBy(episodes, function(x) { return x.episode; });
+            $scope.episodesById[seriesId][seasonNum] = episodeMap;
+          });
+        }
+      };
+
+      $scope.onEpisodeClick = function(seriesId, seasonNum, episodeNum) {
+
+        var episode = $scope.episodesById[seriesId][seasonNum][episodeNum];
+        if (!episode || !episode.available)
+          return;
+
+        var request = $http({
+          method: "POST",
+          url: "http://localhost:8080/series",
+          headers: {'Content-Type': 'application/json'},
+          data: {
+            seriesId: seriesId,
+            season: seasonNum,
+            episode: episodeNum
+          }
+        });
+
+        request.success(function(data) {
+            episode.seen = !episode.seen;
+          });
+
+        // If the episode is available, toggle the seen flag
+        //$http.get('http://localhost:8080/series/' + seriesId + '/' + seasonNum + '/' + episodeNum + '/state').success(function(data) {
+        //  episode.seen = !episode.seen;
+        //});
+      }
+
+    }]);
